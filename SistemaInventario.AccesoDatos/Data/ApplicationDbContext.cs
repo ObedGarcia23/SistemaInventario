66fae4bed43caa9edaff9ea78185e8480e3f475a
obed garcia <obedgc_@hotmail.com>
2024-01-29 15:55:10 -0600

    Configuracion de Proyecto

---
+++ SistemaInventario.AccesoDatos/Data/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SistemaInventario.Modelos.ViewModels;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace SistemaInventario.AccesoDatos.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Bodega> Bodegas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+
+        {
+            base.OnModelCreating(builder);
+
+            // Configure primary key for IdentityUserLogin<string>
+            builder.Entity<IdentityUserLogin<string>>(entity =>
+            {
+                entity.HasKey(e => new { e.LoginProvider, e.ProviderKey });
+            });
+
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
